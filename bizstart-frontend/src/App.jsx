fca939be2a8c04207fc14ceef495893c03cd41f2
starr365 <mmesomanzeribe@gmail.com>
2026-02-13 12:45:53 +0100

    First commit

---
+++ bizstart-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-black text-red-500 flex items-center justify-center">
+      <h1 className='text-4xl md:text-6xl font-bold uppercase hover:text-blue-500 transition-all hover:underline '>
+        Landing page goes here
+      </h1>
+    </div>
+  )
+}
+
+export default App
